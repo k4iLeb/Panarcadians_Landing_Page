75b0e27d654b410a4161bfdcbc05204b4b3f6080
k4iLeb <40836142+k4iLeb@users.noreply.github.com>
2026-07-30 19:20:47 +0300

    Moved dropdown functionality on CSS + transition

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
 import "./style.css";
-
-// *** SELECTORS ***
-
-const menu1 = document.querySelector(".menu-list li:first-child");
-const dropdown = document.querySelector(".dropdown");
-
-// *** EVENT LISTENER ***
-
-menu1.addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  dropdown.classList.add("show");
-});
